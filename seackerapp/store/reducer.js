84a058f794bddfc97c775f71b3fdb6a518e8fb6c
indinabilah <indinabilahaulia@gmail.com>
2019-08-25 11:02:57 +0700

    dummy one

--- seackerapp/store/reducer.js
+++ seackerapp/store/reducer.js
@@ -1,11 +1,12 @@
 const OneSeat = {
-    
+    blockSeat: ['A', 'B', 'C'],
+    numberSeat: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 }
 const Meeting = {
 
 }
 const Auditorium = {
-    
+
 }
 
 const dataStore = {

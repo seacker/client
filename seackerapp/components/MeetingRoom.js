c1acb36cf0d4bd1ef9c362551928c74075f249dd
Stephen Lie <stephenlie55@yahoo.com>
2019-08-27 14:41:56 +0700

    Meeting room tambah fitur book, !done

--- seackerapp/components/MeetingRoom.js
+++ seackerapp/components/MeetingRoom.js
@@ -1,18 +1,153 @@
-import React, {useState} from 'react'
-import {View, Text, Button, Modal, TouchableOpacity, FlatList} from 'react-native'
+import React, {useState, useEffect} from 'react'
+import {View, Text, Button, Modal, TouchableOpacity, FlatList, AsyncStorage, Alert} from 'react-native'
 // import ImageViewer from 'react-native-image-zoom-viewer';
 import DatePicker from 'react-native-datepicker'
+import axios from 'axios'
 
 const MeetingRoom = (props) => {
     // change image, showing or not
-
     const [date, setDate] = useState(`${new Date().getFullYear()}-${new Date().getMonth()+1}-${new Date().getDate()}`)
     const [minMinutes, setMinutes] = useState(new Date().getHours())
     const [minHours, setHours] = useState(new Date().getMinutes())
+    const [schedules, setSchedules] = useState([])
     const [change, setchange] = useState(false)
+    const [book, setBook] = useState({})
+    let listRoom = {
+        A: {
+            index: 'A',
+            color: 'gainsboro',
+            enabled: true
+        },
+        B: {
+            index: 'B',
+            color: 'gainsboro',
+            enabled: true
+        },
+        C: {
+            index: 'C',
+            color: 'gainsboro',
+            enabled: true
+        },
+        D: {
+            index: 'D',
+            color: 'gainsboro',
+            enabled: true
+        }
+    }
+
     const changeModal = (value) => {
         console.log('masuk changeModal ', value)
         setchange(value)
+    }
+
+    retrieveToken = async () => {
+        try {
+          const token = await AsyncStorage.getItem('TASKS');
+          if (value !== null) {
+            // We have token!!
+            axios({
+                method: 'get',
+                url: 'http://localhost:3000/booking',
+                headers: {
+                    token
+                }
+            })
+            .then( ({data}) => {
+                setSchedules(data)
+            })
+            .catch( (err) => {
+                console.log(err)
+                Alert.alert(
+                    'Error fetch data from server',
+                    `Detail: ${err.message}`,
+                    [
+                        {
+                          text: 'Cancel',
+                          onPress: () => console.log('Cancel Pressed'),
+                          style: 'cancel',
+                        },
+                        {
+                          text: 'OK', 
+                          onPress: () => console.log('OK Pressed')
+                        },
+                    ],
+                    {cancelable: false},
+                )
+            })
+          }
+        } catch (error) {
+          // Error retrieving token
+          Alert.alert(
+            'Request rejected',
+            'Access token is required to access data from server!',
+            [
+              {
+                text: 'Cancel',
+                onPress: () => console.log('Cancel Pressed'),
+                style: 'cancel',
+              },
+              {
+                text: 'OK', 
+                onPress: () => console.log('OK Pressed')},
+            ],
+            {cancelable: false},
+          );
+        }
+      };
+
+    useEffect( () => {
+        retrieveToken()
+    }, [])
+
+    useEffect( () => {
+        book.forEach( (room) => {
+            for (var key in listRoom) {
+                if (room == key) {
+                    listRoom[key].color = 'red'
+                    listRoom[key].enabled = false
+                }
+            }
+        })
+    }, [book])
+    
+    useEffect( () => {
+        console.log(schedules)
+    }, [schedules])
+
+    function bookRoom(oneRoom) {
+        book.forEach( (room) => {
+            if (room !== oneRoom ) {
+                setBook([...book, oneRoom])
+            } else {
+                var newBook = book.filter((rooom) => {
+                    if (rooom !== oneRoom) {
+                        return rooom
+                    }
+                })
+                setBook(newBook)
+            }
+        })
+    }
+    
+
+    function nestedFlatlist(rooms) {
+        return (
+            <View style={{marginLeft: 100}}>
+                <FlatList
+                    data={rooms}
+                    renderItem={({item}) => (
+                        <View>
+                            <Text>
+                                item.name
+                            </Text>
+                            <Text>
+                                item.taker
+                            </Text>
+                        </View>
+                    )}
+                />
+            </View>
+        )
     }
 
     return (
@@ -92,29 +227,43 @@ const MeetingRoom = (props) => {
                         />
                     </View>
                     <View>
-                        <TouchableOpacity>
-                            <View style={{height: 50, width: 50, background: 'gainsboro'}}>
+                        <FlatList
+                            data={schedules}
+                            renderItem={({item}) => (
+                                <View style={{flexDirection: 'column'}}>
+                                    <Text>Date: {item.date}, </Text>
+                                    <Text>Start: {item.startBook}, </Text>
+                                    <Text>End: {item.endBook}, </Text>
+                                    <Text>Booker: {userBook}</Text>
+                                    {nestedFlatlist(item.arrRooms)}
+                                </View>
+                            )}
+                        />
+                    </View>
+                    <View style={{marginTop: 10, flexDirection: 'column'}}>
+                        <TouchableOpacity disabled={listRoom.A.enabled} onPress={() => {bookRoom('A')}}>
+                            <View style={{height: 50, width: 50, background: listRoom.A.color}}>
                                 <Text>
                                     A    
                                 </Text>                            
                             </View>
                         </TouchableOpacity>
-                        <TouchableOpacity>
-                            <View style={{height: 50, width: 50, background: 'gainsboro'}}>
+                        <TouchableOpacity disabled={listRoom.B.enabled} onPress={() => {bookRoom('B')}}>
+                            <View style={{height: 50, width: 50, background: listRoom.B.color}}>
                                 <Text>
                                     B
                                 </Text>                            
                             </View>
                         </TouchableOpacity>
-                        <TouchableOpacity>
-                            <View style={{height: 50, width: 50, background: 'grey'}}>
+                        <TouchableOpacity disabled={listRoom.C.enabled} onPress={() => {bookRoom('C')}}>
+                            <View style={{height: 50, width: 50, background: listRoom.C.color}}>
                                 <Text>
                                     C 
                                 </Text>                          
                             </View>
                         </TouchableOpacity>
-                        <TouchableOpacity>
-                            <View style={{height: 50, width: 50, background: 'grey'}}>
+                        <TouchableOpacity disabled={listRoom.D.enabled} onPress={() => {bookRoom('D')}}>
+                            <View style={{height: 50, width: 50, background: listRoom.D.color}}>
                                 <Text>
                                     D    
                                 </Text>                            
@@ -122,47 +271,7 @@ const MeetingRoom = (props) => {
                         </TouchableOpacity>
                     </View>
                     <View>
-                        <FlatList
-                            data={[
-                                {
-                                    number: 1, start: '09.30', end: '10.30', 
-                                    rooms: [{name: 'A', taker: 'Andi'}, {name: 'B', taker: 'Andi'}, {name: 'C', taker: null}, {name: 'D', taker: null}]
-                                },
-                                {
-                                    number: 2, start: '10.31', end: '12.30', 
-                                    rooms: [{name: 'A', taker: null}, {name: 'B', taker: 'Mike'}, {name: 'C', taker: 'Mike'}, {name: 'D', taker: null}]
-                                },
-                                {
-                                    number: 1, start: '14.30', end: '16.30', 
-                                    rooms: [{name: 'A', taker: null}, {name: 'B', taker: null}, {name: 'C', taker: 'Novita'}, {name: 'D', taker: 'Novita'}]
-                                }
-                            ]}
-                            renderItem={({item}) => (
-                                <View style={{flexDirection: 'column'}}>
-                                    <Text>{item.number}</Text>
-                                    <Text>{item.start}</Text>
-                                    <Text>{item.end}</Text>
-                                    <View style={{marginLeft: 100}}>
-                                        <FlatList
-                                            data={item.rooms}
-                                            renderItem={({item}) => (
-                                                <View>
-                                                    <Text>
-                                                        item.name
-                                                    </Text>
-                                                    <Text>
-                                                        item.taker
-                                                    </Text>
-                                                </View>
-                                            )}
-                                        />
-                                    </View>
-                                </View>
-                            )}
-                        />
-                    </View>
-                    <View>
-                        <TouchableOpacity>
+                        <TouchableOpacity onPress={() => {sendRequest(book, userId, )}}>
                             <View style={{background: 'blue', height: 50, width: 100, position: 'absolute', bottom: 10, right: 10}}>
                                 <Text>
                                     Book
